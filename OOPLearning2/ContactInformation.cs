d024c311f9bbb16208b2d641e307c032619ef2d8
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-02-26 11:35:22 +0100

    Added validation methods for ContactInformation

--- OOPLearning2/ContactInformation.cs
+++ OOPLearning2/ContactInformation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace OOPLearning2
@@ -14,7 +15,84 @@ namespace OOPLearning2
             Mail = mail;
             Phone = phone;
         }
-        public string Mail { get { return mail; } set {} }
-        public string Phone { get { return phone; } set {  } }
+        public string Mail 
+        {
+            get { return mail; }
+            set
+            {
+                (bool isValid, string errorMessage) validationResult = ValidateMail(value);
+                if(!validationResult.isValid)
+                {
+                    throw new ArgumentException(nameof(mail), validationResult.errorMessage);
+                }
+                if(value != mail)
+                {
+                    mail = value;
+                }
+            }
+        }
+        public string Phone {
+            get { return phone; } 
+            set 
+            {
+                (bool isValid, string errorMessage) validationResult = ValidatePhone(value);
+                if(!validationResult.isValid)
+                {
+                    throw new ArgumentException(nameof(phone), validationResult.errorMessage);
+                }
+                if(value != phone)
+                {
+                    phone = value;
+                }
+            } 
+        }
+
+        public static (bool, string) ValidateMail(string mail)
+        {
+            if(mail is null)
+            {
+                return (false, "Mail er null!");
+            }
+            if(mail.Any(c => char.IsWhiteSpace(c)))
+            {
+                return (false, "Mail må ikke indeholde whitespaces!");
+            }
+            if(!mail.Contains("@"))
+            {
+                return (false, "Mail skal indeholde et '@'!");
+            }
+            if(!mail.Contains("."))
+            {
+                return (false, "Mail skal indeholde et domæne! f.eks '.com'");
+            }
+            else
+            {
+                return (true, String.Empty);
+            }
+        }
+
+        public static (bool, string) ValidatePhone(string phone)
+        {
+            if(phone is null)
+            {
+                return (false, "Phone er null!");
+            }
+            if(!phone.Any(c => char.IsNumber(c)))
+            {
+                return (false, "Phone må kun indeholde tal!");
+            }
+            if(phone.Length < 4)
+            {
+                return (false, "Phone må ikke være mindre end 4!");
+            }
+            if(phone.Length > 15)
+            {
+                return (false, "Phone må ikke være længere end 15!");
+            }
+            else
+            {
+                return (true, String.Empty);
+            }
+        }
     }
 }
