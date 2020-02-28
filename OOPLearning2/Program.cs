83a86e26b18cd3a89d2e04f1906394aa9b598833
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-02-28 10:55:13 +0100

    Improved the create address method

--- OOPLearning2/Program.cs
+++ OOPLearning2/Program.cs
@@ -6,19 +6,22 @@ namespace OOPLearning2
 {
     class Program
     {
-        static Person richard;
+       
+        static List<Person> persons = new List<Person>();
 
         static void Main()
         {           
             try
             {               
-                createPerson("Richard", "Head", new DateTime(1969, 9, 6), new ContactInformation("Headboss@mail.co.ck", "42069666"));          
+                createPerson("Richard", "Head", new DateTime(1969, 9, 6), new ContactInformation("Headboss@mail.co.ck", "42069666"));
+                createPerson("Ivan", "Jerkov", new DateTime(2000, 04, 20), new ContactInformation("jerkov@mail.ru", "12345678"));
+                createAddress("420th street", "69", "1234", "Intercourse", "USA", persons = new List<Person>() {persons[0]});
             }
             catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            createAddress("420th street", "69", "1234", "Intercourse", "USA", new List<Person>() { richard });
+            
             Console.ReadLine();
         }
 
@@ -26,7 +29,8 @@ namespace OOPLearning2
         {
                 TimeSpan timeSpanAge = DateTime.Now.Subtract(birthDate);
                 int age = timeSpanAge.Days / 365;
-                richard = new Person(firstName, lastName, birthDate, age, contactInformation);
+                Person person = new Person(firstName, lastName, birthDate, age, contactInformation);
+                persons.Add(person);
                 Console.WriteLine("Personer: ");
                 Console.WriteLine($"Fornavn: {firstName}");
                 Console.WriteLine($"Efternavn: {lastName}");
@@ -36,7 +40,7 @@ namespace OOPLearning2
                 Console.WriteLine($"Telefonnummer: {contactInformation.Phone}\n");           
         }
 
-        private static void createAddress(string streetName, string streetNumber, string zip, string city, string country, List<Person> persons) 
+        private static void createAddress(string streetName, string streetNumber, string zip, string city, string country, List<Person> people) 
         {
             Address address = new Address(streetName, streetNumber, zip, city, country, persons);
             Console.WriteLine("Adresser: ");
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace tests
         [Fact]
         public void Test1()
         {
-            Person person = new Person("Ivan", "Jerkov", new DateTime(2000, 04, 20), -1, new ContactInformation("jerkov@mail.ru", "12345678"));
+            
             Address address = new Address("gade", "1", "72", "Vejle", "Danmark", new List<Person>() { });         
         }
     }
